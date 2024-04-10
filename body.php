915cc2c21d3a71283293c5628cd1bbb1526d56f5
@jehovahsays <morgansbyers@gmail.com>
2024-04-10 03:27:33 -0400

    Create body.php
    a command line is typically self-documenting, stating exactly what the user wants done.

---
+++ body.php
@@ -0,0 +1,9 @@
+<body bgcolor="gray">
+</br>
+	<form action="/index.php?action=post" method="post">
+       <input type="text" name="username" placeholder="C:\sql.log"\>	 
+  </form>
+</br>
+<iframe color="gray" width="300px" height="100px" src="./sql.log" frameborder="0">
+</iframe>
+</body>
