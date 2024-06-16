09229ffc7c7334974e5bac8a8cb26a4926dadad8
@jehovahsays <morgansbyers@gmail.com>
2024-06-16 02:27:45 -0400

    restructured the architecture

--- action.php
+++ action.php
@@ -1,5 +1,4 @@
 <?php
-// do not visit this page directly in browser each visit will erase 1 line from manaual.json file breaking the sidebar menu
 header_remove( 'X-Powered-By' );
 header( 'Cache-control: none, no-cache, private, max-age=0' );
 header( 'Pragma: no-cache' );
@@ -19,25 +18,20 @@ header( 'Expires: 0' );
 header( 'Referrer-Policy:  same-origin' );
 header( 'Accept-Language: en-US,en;q=0.5' );
 header( 'Connection: Keep-alive' );
-header( 'Location: ./index.html#en/index' );
-foreach($_POST as $variable => $value) {
-    $value = str_replace(' ', '_', $value);
-	$answer1 = $_POST['secure-form-answer'];        
-	$totalCorrect = 0;           
-	if ($answer1 == "Human") { $totalCorrect++; }            
-	echo "<div id='results'>$totalCorrect /  1 correct</div>";    
+header( 'Location: ./index.html' );
+            $answer1 = $_POST['secure-form-answer'];        
+            $totalCorrect = 0;           
+            if ($answer1 == "Human") { $totalCorrect++; }            
+            echo "<div id='results'>$totalCorrect /  1 correct</div>";
+	foreach($_POST as $variable => $value) {
+    $value = str_replace(' ', '_', $value);			
 	$handle = fopen("./en/" . $value . ".html", "a");
 	fwrite($handle, "\n" 
 	. "<!DOCTYPE html>" 
-	. "<meta name=" . "\"" . "viewport" . "\"" . "content=" 
-	. "\"" . "user-scalable=yes, initial-scale=1.0, maximum-scale=1.0" . "\"" . "/>" 
-	. "<title>" 
-	. $value 
-	. "</title>" 
-	. "</br>"
-	. $value
-	. "<br><br>\n" 
-	. "<form action=\"../server.php\" method=\"post\">\n" 
+	. "<meta name=" . "\"" . "viewport" . "\"" . "content=" . "\"" . "user-scalable=yes, initial-scale=1.0, maximum-scale=1.0" . "\"" . "/>" 
+	. "<title>" . $value . "</title>"
+    . $value	
+	. "<br><br><form action=\"../action.php\" method=\"post\">\n" 
 	. "<input id=\"secure-form-answer-Human\" type=\"text\" name=\"secure-form-answer\" \n"
 	. "onkeypress=\"return event.charCode != 32\" \n" 
 	. "style=\"width: 150px;height: 30px;\" maxlength=\"40\" autocomplete=\"true\" \n"
@@ -49,30 +43,14 @@ foreach($_POST as $variable => $value) {
 	. "\""
 	. ">Human</label></noscript>"
 	. "</form>\n" 
+	. "\n" 
+	. "<br><a href=" . "\"" . "../delete.php?action=delete&filename=./en/" . $value . ".html" . "\"" . ">delete this page</a>"
+	. "<center>\n" 
+	. "<br><a href=\"../index.html\"class=\"handle\">return to homepage</a></br>\n" 
 	. "\n"
     . "\r\n");
-}
-$handle = fopen("./manual.json", "a");
-// load the data and delete the line from the array 
-$lines = file('./manual.json'); 
-$last = sizeof($lines) - 1 ; 
-unset($lines[$last]); 
-// write the new data to the file 
-file_put_contents('./manual.json', $lines); 
-foreach($_POST as $variable => $value) {
-    $value = str_replace(' ', '_', $value);
-	fwrite($handle, 
-	","
-	. "\"" 
-	. $value 
-	. "\"" 
-	. ":" 
-	. "\"" 
-	. "en/"
-	. $value 
-	. "\""
-    . "\n}}}"
-    . "\r\n");
+$handle = fopen("./index.html", "a");
+	fwrite($handle, "<a href=" . "\"" . "./en/" . $value . ".html" . "\"" . "class=" . "\"" . "handle" . "\"" . "><button>" . $value . "</button>" . "</a></br>" . "\r\n");
 }
 fclose($handle);
 ?>
