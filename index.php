380975ead7bf54761d8e455738a0e1dab69d06c4
@jehovahsays <morgansbyers@gmail.com>
2024-06-17 13:30:46 -0400

    adding navigation menu template

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+header_remove( 'X-Powered-By' );
+header( 'Cache-control: none, no-cache, private, max-age=0' );
+header( 'Pragma: no-cache' );
+header( 'Content-Type-Options: nosniff' );
+header( 'X-Content-Type-Options: nosniff' );
+header( 'XSS-Protection: 1; mode=block' );
+header( 'X-XSS-Protection: 1; mode=block' );
+header( 'Vary: Accept-Encoding' );
+header( 'viewport: width=device-width, initial-scale=1.0' );
+header( 'Accept-Language: en-US,en;q=0.5' );
+header( 'Connection: Keep-alive' );
+header( 'Host: handle' );
+header( 'description: handle' );
+header( 'keywords: handle' );
+header( 'Vary: Accept-Encoding' );
+header( 'Expires: 0' );
+header( 'Referrer-Policy:  same-origin' );
+header( 'Accept-Language: en-US,en;q=0.5' );
+header( 'Connection: Keep-alive' );
 header( 'Location: ./index.html' );
 ?>
